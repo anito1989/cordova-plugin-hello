957a9204f7d08e5c6d8232c361f68e1b02300eff
anito1989 <37519871+anito1989@users.noreply.github.com>
2018-07-31 11:58:32 +1200

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,27 +3,85 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+
+import android.content.res.AssetManager;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
+import com.honeywell.mobility.print.LinePrinter;
+import com.honeywell.mobility.print.LinePrinterException;
+import com.honeywell.mobility.print.PrintProgressEvent;
+import com.honeywell.mobility.print.PrintProgressListener;
+
 public class Hello extends CordovaPlugin {
+    Context context;
+    AssetManager assetManager;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-        if (action.equals("greet")) {
+        context = this.cordova.getActivity().getApplicationContext();
+        assetManager = this.cordova.getActivity().getAssets();
+        String debugTrace = "";
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
 
-            return true;
+                try {
+                debugTrace += " Reading Settings!;";
+                String jsonCmdAttribStr = loadPrintSettings(assetManager);
+                debugTrace += " Finished reading!;";
 
-        } else {
-            
-            return false;
 
-        }
+                // Setup context
+                debugTrace += " Setting up extra setting!;";
+                LinePrinter.ExtraSettings exSettings = new LinePrinter.ExtraSettings();
+                exSettings.setContext(context);
+                debugTrace += " Done Setting up extra setting!;";
+                }
+                catch (Exception e) {
+                    StringWriter writer = new StringWriter();
+                    PrintWriter printWriter = new PrintWriter(writer);
+                    e.printStackTrace(printWriter);
+                    String errMsg = e.getMessage();
+                    String stackTrace = writer.toString();
+                    printWriter.flush();
+
+                    PluginResult resultB = new PluginResult(PluginResult.Status.Error, debugTrace + " - " + errMsg + " - " + stackTrace);
+                    callbacks.sendPluginResult(resultB);            
+                }
+            }
+        });
+
+        PluginResult pluginResult = new  PluginResult(PluginResult.Status.NO_RESULT); 
+        pluginResult.setKeepCallback(true); // Keep callback
+
+        return true;
     }
+
+    private String loadPrintSettings(AssetManager assetManager) {
+
+        InputStream input = null;
+        ByteArrayOutputStream output = null;
+        input = assetManager.open("www/files/printer_profiles.JSON");
+        output = new ByteArrayOutputStream(8000);
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = input.read(buf)) > 0) {
+            output.write(buf, 0, len);
+        }
+        input.close();
+        input = null;
+
+        output.flush();
+        output.close();
+        output = null;
+        return output.toString();
+    }
+
 }
