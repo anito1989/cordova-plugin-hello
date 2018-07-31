7ff72102354163ccfd1ae3c89013bb2dbf819455
anito1989 <37519871+anito1989@users.noreply.github.com>
2018-07-31 15:42:35 +1200

    Update hello.js

--- www/hello.js
+++ www/hello.js
@@ -3,5 +3,11 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
-    }
+    },
+    getDebugTrace: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "getDebugTrace");
+    },
+    clearDebugTrace: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "clearDebugTrace");
+    },
 };
