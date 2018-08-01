b2b722bc683191ce003ac790aa744201cc3e8174
anito1989 <37519871+anito1989@users.noreply.github.com>
2018-08-01 16:20:40 +1200

    Update hello.js

--- www/hello.js
+++ www/hello.js
@@ -1,13 +1,46 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+
+    connect: function (printerName, macAdressString, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "connect", [printerName, macAdressString]);
     },
+
+    close: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "close");
+    },
+
+    getStatus: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "getStatus");
+    },
+
+    //General commands
+    printGraphicBase64: function (imageString, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "printGraphicBase64", [imageString]);
+    },
+    setBold: function (boolean, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "printGraphicBase64", [boolean]);
+    },
+    write: function (string, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "printGraphicBase64", [string]);
+    },
+    newLine: function (numberOfLines, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "printGraphicBase64", [numberOfLines]);
+    },
+    formFeed: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "printGraphicBase64");
+    },
+
+
+
+
+    //Debug commands
     getDebugTrace: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "getDebugTrace", [null]);
     },
     clearDebugTrace: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "clearDebugTrace", [null]);
     },
+
+
 };
